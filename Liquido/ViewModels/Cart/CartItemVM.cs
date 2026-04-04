30624a047962ba482a0d1bd121d841e05f21f368
Lelofant <libragainsboro@gmail.com>
2026-04-04 12:26:36 +0300

    Add Cart ViewModels (CartViewModel, CartItemViewModel)

---
+++ Liquido/ViewModels/Cart/CartItemVM.cs
@@ -0,0 +1,18 @@
+﻿namespace Liquido.ViewModels.Cart
+{
+    public class CartItemVM
+    {
+        public int CartItemId { get; set; }
+        public int ProductId { get; set; }
+        public string? ProductName { get; set; }
+        public string? ProductImageUrl { get; set; } 
+        public string? Volume { get; set; }
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal SubTotal => Price * Quantity;
+
+        public int MaxQuantity { get; set; }
+    }
+}
---
+++ Liquido/ViewModels/Cart/CartVM.cs
@@ -0,0 +1,11 @@
+﻿namespace Liquido.ViewModels.Cart
+{
+    public class CartVM
+    {
+        public IEnumerable<CartItemVM> Items { get; set; } = new List<CartItemVM>();
+
+        public decimal TotalPrice => Items.Sum(i => i.SubTotal);
+        public int TotalItems => Items.Sum(i => i.Quantity);
+        public bool IsEmpty => !Items.Any();
+    }
+}
