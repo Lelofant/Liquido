7110f609ce71efc813d213fb4749f5ad7911fd9b
Lelofant <libragainsboro@gmail.com>
2026-04-05 20:34:40 +0300

    Add Category service interface

---
+++ Liquido/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using Liquido.Models;
+using Liquido.ViewModels.Products;
+
+namespace Liquido.Services.Interfaces
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<IEnumerable<CategoryOptionVM>> GetAllAsOptionsAsync();
+        Task<Category?> GetByIdAsync(int id);
+        Task CreateAsync(string name, string? description, string? imageUrl);
+        Task UpdateAsync(int id, string name, string? description);
+        Task DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+    }
+}
