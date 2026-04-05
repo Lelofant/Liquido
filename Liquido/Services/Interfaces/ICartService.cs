fc5b20e8e7b2eb25f2782a405e2117684ec788c0
Lelofant <libragainsboro@gmail.com>
2026-04-05 20:26:55 +0300

    Add cart service interface

---
+++ Liquido/Services/Interfaces/ICartService.cs
@@ -0,0 +1,14 @@
+﻿using Liquido.ViewModels.Cart;
+
+namespace Liquido.Services.Interfaces
+{
+    public interface ICartService
+    {
+        Task<CartVM> GetCartAsync(string userId);
+        Task AddToCartAsync(string userId, int productId, int quantity);
+        Task UpdateCartItemAsync(string userId, int cartItemId, int quantity);
+        Task RemoveFromCartAsync(string userId, int cartItemId);
+        Task ClearCartAsync(string userId);
+        Task<int> GetItemCountAsync(string userId); 
+    }
+}
