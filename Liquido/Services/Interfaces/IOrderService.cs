dbb9d2e2748e111fa13b5b3288e6a79d5fc9016a
Lelofant <libragainsboro@gmail.com>
2026-04-05 20:35:06 +0300

    Add Order service interface

---
+++ Liquido/Services/Interfaces/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using Liquido.ViewModels.Orders;
+
+namespace Liquido.Services.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<int> PlaceOrderAsync(string userId, CheckoutVM model);
+        Task<IEnumerable<OrderSummaryVM>> GetUserOrdersAsync(string userId);
+        Task<OrderSummaryVM?> GetOrderDetailsAsync(int orderId, string userId);
+        Task<IEnumerable<OrderSummaryVM>> GetAllOrdersAsync();
+        Task<OrderSummaryVM?> GetOrderDetailsAdminAsync(int orderId);
+        Task UpdateStatusAsync(int orderId, string status);
+    }
+}
