46937e809b6e5724ece29f0f54d94e2953b8acf5
Lelofant <libragainsboro@gmail.com>
2026-04-05 20:35:29 +0300

    Add Review service interface

---
+++ Liquido/Services/Interfaces/IReviewService.cs
@@ -0,0 +1,15 @@
+﻿using Liquido.ViewModels.Reviews;
+
+namespace Liquido.Services.Interfaces
+{
+    public interface IReviewService
+    {
+        Task CreateAsync(string userId, ReviewsFormVM model);
+        Task DeleteAsync(int reviewId, string userId);
+        Task<IEnumerable<ReviewsVM>> GetApprovedForProductAsync(
+            int productId, string? currentUserId);
+        Task<IEnumerable<ReviewsVM>> GetPendingAsync();
+        Task ApproveAsync(int reviewId);
+
+    }
+}
