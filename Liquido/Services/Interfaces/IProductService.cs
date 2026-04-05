ece5fd96d01018a8aa1d58e3017acd0c79171005
Lelofant <libragainsboro@gmail.com>
2026-04-05 20:35:17 +0300

    Add Product service interface

---
+++ Liquido/Services/Interfaces/IProductService.cs
@@ -0,0 +1,27 @@
+﻿using Liquido.ViewModels.Products;
+
+namespace Liquido.Services.Interfaces
+{
+    public interface IProductService
+    {
+        public interface IProductService
+        {
+            Task<ProductListVM> GetPagedAsync(
+                string? searchTerm,
+                int? categoryId,
+                decimal? minPrice,
+                decimal? maxPrice,
+                int page,
+                int pageSize);
+
+            Task<ProductListVM> GetDetailsByIdAsync(int id);
+            Task<IEnumerable<ProductListVM>> GetFeaturedAsync(int count = 6);
+            Task<int> CreateAsync(ProductFormVM model);
+            Task UpdateAsync(int id, ProductFormVM model);
+            Task DeleteAsync(int id);
+            Task<bool> ExistsAsync(int id);
+            Task<ProductFormVM> GetFormModelAsync(int? id = null);
+        }
+
+    }
+}
