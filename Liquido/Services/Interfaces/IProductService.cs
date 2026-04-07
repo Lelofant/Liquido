957bd508d67a5a86b5ee2f90cb474dc8f916275f
Lelofant <libragainsboro@gmail.com>
2026-04-07 15:23:54 +0300

    Fixing IProductService

--- Liquido/Services/Interfaces/IProductService.cs
+++ Liquido/Services/Interfaces/IProductService.cs
@@ -3,9 +3,7 @@
 namespace Liquido.Services.Interfaces
 {
     public interface IProductService
-    {
-        public interface IProductService
-        {
+    {    
             Task<ProductListVM> GetPagedAsync(
                 string? searchTerm,
                 int? categoryId,
@@ -13,15 +11,13 @@ namespace Liquido.Services.Interfaces
                 decimal? maxPrice,
                 int page,
                 int pageSize);
-
-            Task<ProductListVM> GetDetailsByIdAsync(int id);
-            Task<IEnumerable<ProductListVM>> GetFeaturedAsync(int count = 6);
+        
+            Task<ProductDetailsVM?> GetDetailsByIdAsync(int id);
+            Task<IEnumerable<ProductVM>> GetFeaturedAsync(int count = 6);
             Task<int> CreateAsync(ProductFormVM model);
             Task UpdateAsync(int id, ProductFormVM model);
             Task DeleteAsync(int id);
             Task<bool> ExistsAsync(int id);
             Task<ProductFormVM> GetFormModelAsync(int? id = null);
-        }
-
     }
 }
