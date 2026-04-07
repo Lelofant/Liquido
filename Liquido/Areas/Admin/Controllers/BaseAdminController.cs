46e47179fe45e55962179fe75e89da7ec415e134
Lelofant <libragainsboro@gmail.com>
2026-04-07 15:24:35 +0300

    Add BaseAdminController with Admin role authorization

---
+++ Liquido/Areas/Admin/Controllers/BaseAdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Liquido.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class BaseAdminController : Controller
+    {
+      
+    }
+}
