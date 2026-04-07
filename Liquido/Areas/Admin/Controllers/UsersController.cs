fdfc7d06a2ac444e5201d9cc51f0b5aee7625637
Lelofant <libragainsboro@gmail.com>
2026-04-07 15:26:56 +0300

    AddAdmin Users management with activate/deactivate

---
+++ Liquido/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,38 @@
+﻿using Liquido.Data;
+using Liquido.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Liquido.Areas.Admin.Controllers
+{
+    public class UsersController : BaseAdminController
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(UserManager<ApplicationUser> userManager,ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _context.Users.ToListAsync();
+            return View(users);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ToggleActive(string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user is null) return NotFound();
+
+            user.IsActive = !user.IsActive;
+            await _userManager.UpdateAsync(user);
+            return RedirectToAction("Index");
+        }
+    }
+}
