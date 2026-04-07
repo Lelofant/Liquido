f38fa386eebd279e9565377001ee162dc7c53aa0
Lelofant <libragainsboro@gmail.com>
2026-04-07 15:39:35 +0300

    Removing CategoryOptionsVM

--- Liquido/ViewModels/Products/CategoryOptionVM.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Liquido.ViewModels.Products
-{
-    public class CategoryOptionVM
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-    }
-}
